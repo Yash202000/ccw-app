fb074fa02a2e3bd2840f3019cf0fea40c1134444
Yash202000 <yashpanchwatkar@gmail.com>
2023-09-10 11:26:45 +0530

    cors added

--- ccw-server/apps/ccw-server/src/main.ts
+++ ccw-server/apps/ccw-server/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors();
 
   await app.listen(port);
   Logger.log(
