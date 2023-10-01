11b5c5cc7b80aad60417335e32d90f31e7ef4ef2
sahilb16 <sahilbhavsar1611@gmail.com>
2023-10-01 17:52:57 +0530

    I have added the routes in app.tsx

---
+++ ccw-server/apps/ccw-admin/src/pages/complaint/complaint.tsx
@@ -0,0 +1,18 @@
+// Import necessary dependencies
+import React from 'react';
+import { Grid, Button, Box } from '@mui/material';
+
+
+// Define the AdminDashboard component
+const Complaintinfo = () => {
+
+
+  return (
+    <div>
+        Complaint
+    </div>
+   
+  );
+};
+
+export default Complaintinfo;
